9c2cafd0d185baebc5f962575ca5f33d872ec9be
Greg Schueler <greg.schueler@gmail.com>
2019-02-25 11:45:59 -0800

    Add impls

---
+++ src/main/java/org/rundeck/plugins/nodes/attributes/AttributeNodeEnhancer.java
@@ -0,0 +1,150 @@
+package org.rundeck.plugins.nodes.attributes;
+
+
+import com.dtolabs.rundeck.core.common.INodeEntry;
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.core.plugins.configuration.DynamicProperties;
+import com.dtolabs.rundeck.core.plugins.configuration.StringRenderingConstants;
+import com.dtolabs.rundeck.plugins.descriptions.*;
+import com.dtolabs.rundeck.plugins.nodes.IModifiableNodeEntry;
+import com.dtolabs.rundeck.plugins.nodes.NodeEnhancerPlugin;
+import org.rundeck.app.spi.Services;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Plugin(service = "NodeEnhancer", name = org.rundeck.plugins.nodes.attributes.AttributeNodeEnhancer.PROVIDER)
+@PluginDescription(title = "Attribute Match",
+                   description =
+                           "Adds new attributes to a node if a match is found.\n\nIf the attribute exists and "
+                           + "optionally "
+                           + "matches a certain value, add the custom attributes.")
+public class AttributeNodeEnhancer
+        implements NodeEnhancerPlugin
+{
+    public static final String PROVIDER = "attributeMatch";
+
+
+    @PluginProperty(title = "Attribute Matches",
+                    description =
+                            "Attribute name/values to match. All matches must be met to add the additional attributes"
+                            + ".\n\n"
+                            + "Each entry should be of the form:\n"
+                            + "* `key` `operator` `value`\n\n"
+                            + "Where `key` is the attribute name, "
+                            + "`operator` is one of:\n\n"
+                            + "* `==` equality match\n"
+                            + "* `!!` not present match\n"
+                            + "* `=~` regular expression match\n"
+                            + "* `!=` inequality match\n"
+                            + "* `!~` negative regular expression match\n",
+                    required = true)
+    @TextArea
+    private String match;
+
+    /**
+     *
+     */
+    @PluginProperty(title = "Attributes to Add",
+                    description = "Attribute name=value to add to matching nodes, in Java Properties format")
+    @TextArea
+    private String add;
+
+    @PluginProperty(title = "Tags to Add",
+                    description = "Comma-separated tags to add")
+
+    private String addTags;
+
+
+    Map<String, String> loadedProps;
+    Set<String> loadedTags;
+    static final Pattern ComparisonPattern = Pattern.compile(
+            "^(?<key>.+?)(?<op>==|!=|=~|!~|!!)(?<val>.*)$"
+    );
+
+    @Override
+    public void updateNode(
+            final String project, final IModifiableNodeEntry node
+    )
+    {
+        if (!matchesAll(node.getAttributes())) {
+            return;
+        }
+        addAll(node.getAttributes());
+        addAllTags(node.getTags());
+    }
+
+    private void addAllTags(final Set tags) {
+        if (null == loadedTags) {
+            loadedTags = new HashSet<>();
+            if (addTags != null && !"".equals(addTags.trim())) {
+                loadedTags.addAll(Arrays.asList(addTags.split(",\\s*")));
+            }
+        }
+        tags.addAll(loadedTags);
+    }
+
+
+    private void addAll(final Map<String, String> attributes) {
+        if (null == loadedProps) {
+            Map<String, String> map = new HashMap<>();
+            if (add != null && !"".equals(add.trim())) {
+                try {
+                    Properties props = new Properties();
+                    props.load(new StringReader(add));
+                    for (String stringPropertyName : props.stringPropertyNames()) {
+                        map.put(stringPropertyName, props.getProperty(stringPropertyName));
+                    }
+                } catch (IOException e) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            loadedProps = map;
+        }
+        attributes.putAll(loadedProps);
+    }
+
+    private boolean matchesAll(final Map<String, String> attributes) {
+        Map<String, Predicate<String>> comparisons = new HashMap<>();
+        for (final String s : match.split("\r\n")) {
+            Matcher matcher = ComparisonPattern.matcher(s);
+            if (matcher.matches()) {
+                String key = matcher.group("key");
+                String op = matcher.group("op");
+                String val = matcher.group("val");
+                Predicate<String> newOp = makePredicate(op, val);
+                comparisons.compute(key, (s1, stringPredicate) ->
+                        stringPredicate != null ? stringPredicate.and(newOp) : newOp
+                );
+
+            }
+        }
+        for (String s : comparisons.keySet()) {
+            if (!comparisons.get(s).test(attributes.get(s))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Predicate<String> makePredicate(final String op, final String val) {
+        if ("==".equals(op)) {
+            return val::equals;
+        } else if ("!=".equals(op)) {
+            return makePredicate("==", val).negate();
+        } else if ("!!".equals(op)) {
+            return Objects::isNull;
+        } else if ("=~".equals(op)) {
+            Pattern p = Pattern.compile(val);
+            return (s) -> p.matcher(s).matches();
+        } else if ("!~".equals(op)) {
+            return makePredicate("=~", val).negate();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/main/java/org/rundeck/plugins/nodes/icon/IconNodeEnhancer.java
@@ -0,0 +1,130 @@
+package org.rundeck.plugins.nodes.icon;
+
+import com.dtolabs.rundeck.core.common.INodeEntry;
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.core.plugins.configuration.DynamicProperties;
+import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
+import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
+import com.dtolabs.rundeck.plugins.descriptions.SelectLabels;
+import com.dtolabs.rundeck.plugins.descriptions.SelectValues;
+import com.dtolabs.rundeck.plugins.nodes.IModifiableNodeEntry;
+import com.dtolabs.rundeck.plugins.nodes.NodeEnhancerPlugin;
+import org.rundeck.app.spi.Services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+@Plugin(service = "NodeEnhancer", name = IconNodeEnhancer.PROVIDER)
+@PluginDescription(title = "Icon",
+                   description =
+                           "Adds an icon or badges based on an attribute.\n\nIf the attribute exists and optionally "
+                           + "matches a certain value, use the selected icon")
+public class IconNodeEnhancer
+        implements NodeEnhancerPlugin, DynamicProperties
+{
+
+    private static final String[] ALL_NAMES;
+
+    static {
+        Properties iconProperties = new Properties();
+        try {
+            InputStream resourceAsStream = IconNodeEnhancer.class
+                    .getClassLoader()
+                    .getResourceAsStream("org/rundeck/plugins/nodes/icon/IconNodeEnhancer.properties");
+            iconProperties.load(resourceAsStream);
+            String[] GLYPHICON_NAMES = iconProperties.getProperty("glyphicon.names").split(",");
+            for (int i = 0; i < GLYPHICON_NAMES.length; i++) {
+                GLYPHICON_NAMES[i] = "glyphicon-" + GLYPHICON_NAMES[i];
+            }
+            String[] FAICON_NAMES = iconProperties.getProperty("faicon.names").split(",");
+            for (int i = 0; i < FAICON_NAMES.length; i++) {
+                FAICON_NAMES[i] = "fa-" + FAICON_NAMES[i];
+            }
+            String[] all = new String[GLYPHICON_NAMES.length + FAICON_NAMES.length];
+            System.arraycopy(GLYPHICON_NAMES, 0, all, 0, GLYPHICON_NAMES.length);
+            System.arraycopy(FAICON_NAMES, 0, all, GLYPHICON_NAMES.length, FAICON_NAMES.length);
+            ALL_NAMES = all;
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to load resource", e);
+        }
+    }
+
+    public static final String PROVIDER = "icon";
+
+    @PluginProperty(title = "Attribute Name", description = "Attribute name to look for", required = true)
+    private String attributeName;
+    @PluginProperty(title = "Attribute Value",
+                    description = "Exact attribute value to match (optional)",
+                    required = false)
+    private String attributeValue;
+
+    /**
+     *
+     */
+    @PluginProperty(title = "Icon Name",
+                    description =
+                            "Icon name to use, glyphicons start with `glyphicon-` and font-awesome icons start with "
+                            + "`fa-`.\n\nSee [Font-Awesome](https://fontawesome"
+                            + ".com/icons?d=gallery&m=free) and [Glyphicon](https://getbootstrap.com/docs/3"
+                            + ".4/components/)")
+
+    private String iconName;
+    /**
+     *
+     */
+    @PluginProperty(title = "Icon Color", description = "CSS color for the icon (optional)")
+    private String iconColor;
+    /**
+     *
+     */
+    @PluginProperty(title = "Badges",
+                    description = "Icon badges to add to the Node. Each badge should start with `fa-` or "
+                                  + "`glyphicon-`, *or* the selected Icon Family will be used.")
+
+    private List<String> iconBadges;
+
+    @Override
+    public Map<String, Object> dynamicProperties(
+            final Map<String, Object> projectAndFrameworkValues,
+            final Services services
+    )
+    {
+        HashMap<String, Object> values = new HashMap<>();
+        List<String> iconBadges = Arrays.asList(ALL_NAMES);
+        values.put("iconName", iconBadges);
+        return values;
+    }
+
+    @Override
+    public void updateNode(
+            final String project, final IModifiableNodeEntry node
+    )
+    {
+        if (!node.getAttributes().containsKey(attributeName)) {
+            return;
+        }
+        if (attributeValue != null
+            && !"".equals(attributeValue)
+            && !attributeValue.equals(node.getAttributes().get(attributeName))) {
+            return;
+        }
+        node.addAttribute("ui:icon:name", iconName);
+        if (iconColor != null && !"".equals(iconColor)) {
+            node.addAttribute("ui:icon:color", iconColor);
+        }
+        if (iconBadges != null && iconBadges.size() > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (String iconBadge : iconBadges) {
+                if (sb.length() > 0) {
+                    sb.append(",");
+                }
+                if (iconBadge.startsWith("glyphicon-") || iconBadge.startsWith("fa-") || iconBadge.startsWith("fab-")) {
+                    sb.append(iconBadge);
+                }
+            }
+            node.addAttribute("ui:badges", sb.toString());
+        }
+    }
+
+}
